01a9a9f247b4ff43cda160c8bce88f9d8c6ee98d
Rolo Beltrao <rolobeltrao@gmail.com>
2021-02-02 15:26:49 -0300

    REST testado

---
+++ src/main/java/com/rolobeltrao/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.rolobeltrao.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {		
+		return "<h1>REST está funcionando!</h1>";
+	}
+
+}
